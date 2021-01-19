fb07b4cd77dcf9aa7f9b5fd1a5f1d25deba8c38d
Gerard Said <gerard.said@mcast.edu.mt>
2021-01-19 12:54:02 +0100

    Added project

---
+++ Assets/startScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class startScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject.Find("AStarGrid").GetComponent<AstarPath>().enabled = true;
+            GameObject.Find("Robot").GetComponent<customAIMoveScriptGrid>().enabled = true;
+
+        }
+    }
+
+    
+}
